84d771e333be21255ed8f9f0e99a5a8066d86a8a
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2026-01-28 10:57:20 -0500

    tweaks to equipment generation, new stat, new equipment templates

--- Assets/Scripts/Environment/EquipmentChest.cs
+++ Assets/Scripts/Environment/EquipmentChest.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 
 public class EquipmentChest : Chest
 {
+    public EquipmentDropTable overrideDropTable;
     public override void AcquireLoot()
     {
         Manager m = Manager.instance;
         LootManager lm = LootManager.Instance;
-        Equipment loot = lm.GetEquipmentDropTableForFloor(Manager.instance.currentFloor).GetEquipment();
+        Equipment loot = null;
+        if (overrideDropTable != null)
+        {
+            loot = overrideDropTable.GetEquipment();
+            Debug.Log("Acquired equipment from override drop table.");
+        }
+        else
+        {
+            loot = lm.GetEquipmentDropTableForFloor(Manager.instance.currentFloor).GetEquipment();
+            Debug.Log("Acquired equipment from floor drop table.");
+        }
         Inventory inventory = FindFirstObjectByType<Inventory>();
         if (inventory != null && loot != null &&!isOpened)
         {
--- Assets/Scripts/Loot/EquipmentTemplate.cs
+++ Assets/Scripts/Loot/EquipmentTemplate.cs
@@ -1,12 +1,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum EquipmentRarity
+{
+    Common,
+    Uncommon,
+    Rare,
+    Epic,
+    Legendary
+}
+
+
 [System.Serializable]
 public class EquipmentTemplateStat
 {
     public StatType stat;
     public float minValue;
     public float maxValue;
+    public bool isInt = true;
 }
 [System.Serializable]
 public class EquipmentTemplateDebuff
@@ -30,25 +41,55 @@ public class EquipmentTemplate : ScriptableObject
     public Equipment baseEquipment;
     public List<EquipmentTemplateStat> stats;
     public List<EquipmentTemplateDebuff> potentialDebuffs;
+    public List<EquipmentTemplateStat> guaranteedStats;
     public int minDebuffs = 0;
     public int maxDebuffs = 1;
     public List<EquipmentTemplateBuff> potentialBuffs;
     public int minBuffs = 0;
     public int maxBuffs = 1;
-
+    public int minStats = 1;
+    public int maxStats = 3;
+    public EquipmentRarity rarity;
     public Equipment GenerateEquipment()
     {
         Equipment newEquipment = Instantiate(baseEquipment);
-        foreach (var stat in stats)
+        int statCount = GetRandomInt(minStats, maxStats);
+
+        for (int i = 0; i < statCount; i++)
         {
-            int value = (int)Random.Range(stat.minValue, stat.maxValue);
-            newEquipment.stats.AddStat(stat.stat, value);
+            EquipmentTemplateStat stat = stats[Random.Range(0, stats.Count)];
+            if (stat.isInt)
+            {
+                int value = (int)Random.Range(stat.minValue, stat.maxValue);
+                newEquipment.stats.AddStat(stat.stat, value);
+            }
+            else
+            {
+                float value = Random.Range(stat.minValue, stat.maxValue);
+                value = Mathf.Round(value * 100f) / 100f; // Round to 2 decimal places
+                newEquipment.stats.AddStat(stat.stat, value);
+            }
         }
+
         int debuffCount = GetRandomInt(minDebuffs, maxDebuffs);
         for (int i = 0; i < debuffCount; i++)
         {
             PlayerDebuffInflictorHolder inflictor = GenerateDebuff();
             newEquipment.AddDebuffInflictor(inflictor);
+        }
+        foreach (var guaranteedStat in guaranteedStats)
+        {
+            if (guaranteedStat.isInt)
+            {
+                int value = (int)Random.Range(guaranteedStat.minValue, guaranteedStat.maxValue);
+                newEquipment.stats.AddStat(guaranteedStat.stat, value);
+            }
+            else
+            {
+                float value = Random.Range(guaranteedStat.minValue, guaranteedStat.maxValue);
+                value = Mathf.Round(value * 100f) / 100f; // Round to 2 decimal places
+                newEquipment.stats.AddStat(guaranteedStat.stat, value);
+            }
         }
         // Generate buffs
         return newEquipment;
